21f5625a514b09b90774c86d6faad3cdcae0c027
James <jmaccrimmon@icloud.com>
2022-09-30 17:48:22 -0600

    coding-exam
    eyah

--- coding-exam2.py
+++ coding-exam2.py
@@ -1,82 +1,37 @@
+import math as m
 def generatingNewList(lst1, lst2):
-    '''
-    Generates a new list based on the following rules:
-        - compare elements in the same index for both lists
-        
-        - if the elements in the same index on lst1
-        and lst2 are both even, add those elements and
-        store the result in the same index in the new list
-                    
-        - if the elements in the same index on lst1
-        and lst2 are both odd, multiply those elements and
-        store the result in the same index in the new list
-        
-        - if one of the element in the same index is even and
-        the other element in the same index is odd, subtract
-        the element in lst2 from lst1
-        
-        For example:
-        lst1   = [ 2,  3, 40, 7]
-        lst2   = [10,  5, 15, 2]
-        newLst = [12, 15, 25, 5]
-        
-    This function will return the new list when both parameters
-    (lst1 and lst2) have the exact same length.
-    If the parameters do not have the same length, the
-    function will return an empty list.
+    if len(lst1) != len(lst2):
+        return []
+    newList = []
+    for z in range(len(lst1)):
+        x=lst1[z]
+        y=lst2[z]
+        if x % 2 == 0 and y % 2 == 0:
+            newList.append(x+y)
+        elif x % 2 == 1 and y % 2 == 1:
+            newList.append(x*y)
+        else:
+            newList.append(x-y)
+    return newList
 
-    Parameters
-    ----------
-    lst1 : non empty list of integers
-        
-    lst2 : non empty list of integers
-        
-
-    Returns
-    -------
-    newLst: a new list considering the rules described above
-            or an empty list if the parameters have a
-            different length
-
-    '''
     
 
 def generateListPrime(lst):
-    '''
-    From the parameter lst, generate a new list containing the numbers of lst
-    that are prime numbers. Prime numbers are numbers greater than 1. 
-    They only have two factors, 1 and the number itself. 
-    This means these numbers cannot be divided by any number other than 1
-    and the number itself without leaving a remainder.
-
-    Parameters
-    ----------
-    lst : list
-        list of positive integer numbers.
-
-    Returns
-    -------
-    A new list containing only the prime numbers presented in the lst parameter
-
-    '''
-    
+    def checkPrime(val):
+        for x in range(2, int(m.sqrt(val))+1):
+            if val%x==0:
+                return False
+        return True
+    newList = []
+    for y in lst:
+        if checkPrime(y)and y != 0 and y != 1:
+            newList.append(y)
+    return newList
 
 def main():
     print("Making the calls of the implemented methods")
-    
-    #call the method generateListPrime
-    #if the method return an empty list you need to print
-    #"No prime number in the list"
-    #if the list returned is not empty you will print the list
-    #that was returned
-    
-    
-    #call the method generatingNewList
-    #if the method returns an empty list you need to print
-    #"Not possible to generate the new list"
-    #if the list returned is not empty, print the list
-    #that was returned
-    
+    print(generatingNewList([2,3,40,7],[10,5,15,2]))
+    print(generateListPrime([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
     
 
 if __name__ == "__main__":
